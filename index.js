182f3b5458c491c681411aeea538ded39adf68e1
David Whitney <codifyamplify@gmail.com>
2022-06-15 11:42:36 -0400

    commit tree 2edaa86bc64786a97c633fff939df6550225ac0b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase()
+}
+// describe('setBestCustomer()', function() {
+//     it('setBestCustomer', function() {
+//       expect(window.bestCustomer).to.equal(undefined);
+
+//       setBestCustomer();
+
+//       expect(window.bestCustomer).to.equal('not bob');
+//     });
+//   });
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = 'rachel'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'samunosuke'
+}
